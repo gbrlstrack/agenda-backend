5c9277bc55a737a88fa5d09f8a04389815dcfdfb
gbrlstrack <44452210+gbrlstrack@users.noreply.github.com>
2025-09-04 13:17:29 +0000

    feat ✨: add express to project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Olá, Mundo!');
+});
