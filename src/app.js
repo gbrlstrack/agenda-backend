4384b147f4fb4ff7a2cc0c7a4c371e621ff1805d
gbrlstrack <gbrlstrack@gmail.com>
2025-09-05 16:44:40 -0300

    fix 🐛: add cors config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const userRoutes = require('./api/routes/userRoutes');
+const cors = require('cors');
 //TODO: construir um middleware de errorHandler
 // const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/v1/users', userRoutes);
 // app.use(errorHandler);
