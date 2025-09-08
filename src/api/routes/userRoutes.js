c89f0cb3d853c71665a82be248dd0f1a5c8bcb45
gbrlstrack <gbrlstrack@gmail.com>
2025-09-08 13:26:58 -0300

    feat ✨: add validator to update route

--- src/api/routes/userRoutes.js
+++ src/api/routes/userRoutes.js
@@ -11,7 +11,7 @@ router.route('/')
 
 router.route('/:id')
     .get(userController.getById)
-    .put(userController.update)
+    .put(validateSchema(userSchema), userController.update)
     .delete(userController.destroy)
 
 module.exports = router
